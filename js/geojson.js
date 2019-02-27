2b88c9d6583c883425add2d1547f67ce006a86a6
meganroessler <mroessler@wisc.edu>
2019-02-27 12:24:24 -0600

    Module 5A assignment completed

--- js/geojson.js
+++ js/geojson.js
@@ -1,5 +1,6 @@
 /*Megan Roessler, Geography 575*/
 
+//Create basemap
 function createMap(){
 	var map = L.map('map', {
 		center: [38, -97],
@@ -13,9 +14,9 @@ function createMap(){
 	getData(map);
 };
 
-//Example 2.4
+//Load geojson data to map
 function getData(map){
-	$.ajax("data/women-in-congress.geojson", {
+	$.ajax("data/women-in-congress1.geojson", {
         dataType: "json",
         success: function(response){
 			 var geojsonMarkerOptions = {
@@ -26,7 +27,7 @@ function getData(map){
                 opacity: 1,
                 fillOpacity: 0.8
             };
-            //create a Leaflet GeoJSON layer and add it to the map
+            //add leaflet GeoJSON to map
             L.geoJson(response, {
 				pointToLayer: function (feature, latlng){
 					return L.circleMarker(latlng, geojsonMarkerOptions);
@@ -36,32 +37,78 @@ function getData(map){
     });
 };
 
-//Example 2.5
-function onEachFeature(feature, layer) {
-    //no property named popupContent; instead, create html string with all properties
-    var popupContent = "";
-    if (feature.properties) {
-        //loop to add feature property names and values to html string
-        for (var property in feature.properties){
-            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
-        }
-        layer.bindPopup(popupContent);
-    };
+
+//Create function to calculate prop symbol radius
+function calcPropRadius(attValue) {
+	var scaleFactor = 50;
+	var area = attValue * scaleFactor;
+	var radius = Math.sqrt(area/Math.PI);
+	
+	return radius;
 };
 
-//function to retrieve the data and place it on the map
+//Import GeoJSON data
 function getData(map){
     //load the data
-    $.ajax("data/women-in-congress.geojson", {
+    $.ajax("data/women-in-congress1.geojson", {
         dataType: "json",
         success: function(response){
-
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(response, {
-                onEachFeature: onEachFeature
-            }).addTo(map);
+            //call function to create proportional symbols
+            createPropSymbols(response, map);
         }
     });
 };
 
+function pointToLayer(feature, latlng){
+	var attribute = "116th_congress";
+	
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#ffffff",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+	};
+	
+	var attValue = Number(feature.properties[attribute]);
+	options.radius = calcPropRadius(attValue);
+	
+	var popupContent = "<p><b>State:</b>" + feature.properties.State + "</p>";
+	
+	layer.bindPopup(popupContent);
+	
+	return layer;
+	createPropSymbols.on('click', pointToLayer);
+};
+
+//Create proportional symbols
+//Add circle markers for point features to the map
+function createPropSymbols(data, map){
+	var attribute = "116th_congress";
+    //create marker options
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#ffffff",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //Create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+			var attValue = Number(feature.properties[attribute]);
+			console.log(feature.properties, attValue)
+			geojsonMarkerOptions.radius = calcPropRadius(attValue);
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    }).addTo(map);
+};
+
+
+
+
+//call function
 $(document).ready(createMap);
